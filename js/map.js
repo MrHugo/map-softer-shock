6143d5d50fb00faae61863bd8c6ee109876412ce
Mr.Hugo <mosieur.hugo@gmail.com>
2017-10-16 20:58:08 +0200

    add a header picture & style correction & remove the first buble logic

--- js/map.js
+++ js/map.js
@@ -17,7 +17,7 @@ var color = d3.scaleThreshold()
 .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)","rgb(8,81,156)","rgb(8,48,107)","rgb(3,19,43)"]);
 
 
-var svg = d3.select(".map-container")
+var svg = d3.select(".worldmap")
 .append("svg")
 .attr("viewBox", "0 0 960 500")
 .attr("preserveAspectRatio", "xMidYMid meet")
